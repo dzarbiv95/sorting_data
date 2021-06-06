1a8567680f886ee3ed10761d96607b740a74ef3f
dzarbiv95 <dzarbiv95@gmail.com>
2021-06-07 00:32:10 +0300

    remove needless print at the flask server

--- flask_server.py
+++ flask_server.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def get_ads():
     top = request.args.get('top', default=None, type=int)
     data = db.fetch_first_words(db.connection(), setup.RESULTS_STEP1_TABLE_NAME, top)
-    print(len(data))
     return json.dumps([row[0] for row in data])
 
 
